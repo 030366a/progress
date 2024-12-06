dde0ea73ebe9d63857fb09d4b72cf9e8e7548a74
030366a <m8j5s1@gmail.com>
2024-12-06 20:52:29 +0900

    Update sizes.c
    format

--- sizes.c
+++ sizes.c
@@ -1,18 +1,18 @@
 /*
-   Copyright (C) 2016 Xfennec, CQFD Corp.
+    Copyright (C) 2016 Xfennec, CQFD Corp.
 
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "sizes.h"
@@ -27,21 +27,26 @@ static const uint64_t  exbibytes = 1024ULL * 1024ULL * 1024ULL *
 
 void format_size(uint64_t size, char *result)
 {
-uint64_t multiplier;
-int i;
+    uint64_t multiplier;
+    int i;
 
-multiplier = exbibytes;
+    multiplier = exbibytes;
 
-for (i = 0 ; i < DIM(sizes) ; i++, multiplier /= 1024) {
-    if (size < multiplier)
-        continue;
-    if (size % multiplier == 0)
-        sprintf(result, "%" PRIu64 " %s", size / multiplier, sizes[i]);
-    else
-        sprintf(result, "%.1f %s", (float) size / multiplier, sizes[i]);
+    for (i = 0; i < DIM(sizes); i++, multiplier /= 1024) {
+        if (size < multiplier) {
+            continue;
+        }
+
+        if (size % multiplier == 0) {
+            sprintf(result, "%" PRIu64 " %s", size / multiplier, sizes[i]);
+        }
+        else {
+            sprintf(result, "%.1f %s", (float) size / multiplier, sizes[i]);
+        }
+
+        return;
+    }
+
+    strcpy(result, "0");
     return;
-}
-
-strcpy(result, "0");
-return;
 }
